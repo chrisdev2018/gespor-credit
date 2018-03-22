d18d6406d731758a5fc108de15b48eba40e643a6
christian <christian.fomekong@gmail.com>
2018-03-22 06:24:46 +0100

    Premier vrai commit avec le squelette thème bien bati

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer ">
+      &copy; Copyright 2018.&nbsp;All Rights Reserved.
+</div>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,97 @@
+
+<nav class="col-sm-3 col-md-3 hidden-xs-down bg-faded sidebar" id="sidebar">
+
+    <div class=" app-title" >Système de gestion du porte-feuille de crédit </div>
+
+    <br><br>
+
+    <hr>
+        <h5 class="menu-title"> <span class="oi oi-menu" ></span> MENU</h5>
+    <hr>
+    <ul class="nav nav-pills flex-column">
+        <a class="nav-link" href="#sousmenudossiers" data-toggle="collapse" ><span class="oi oi-folder" ></span> &nbsp; Dossiers</a>
+        <ul class="collapse " id="sousmenudossiers">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><span class="oi oi-plus" ></span> Nouveau</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><span class="oi oi-book" ></span> Traiter</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#sousmenulist" data-toggle="collapse" aria-expanded="false"><span class="oi oi-list" ></span> Lister les dossiers</a>
+                <ul class="collapse " id="sousmenulist">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" ><span class="oi oi-action-redo" ></span> Arrivés</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> <span class="oi oi-thumb-up" ></span> Accordés</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> <span class="oi oi-thumb-down" ></span> Rejetés</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </ul>
+
+    <ul class="nav nav-pills flex-column">
+        <a class="nav-link" href="#sousmenusuivi" data-toggle="collapse" a > <span class="oi oi-briefcase" ></span>&nbsp; Suivi</a>
+        <ul  class="collapse " id="sousmenusuivi">
+            <li class="nav-item">
+                <a class="nav-link" href="#sousmenutraite" data-toggle="collapse" ><span class="oi oi-pencil" ></span> Payer une traite</a>
+                <ul class="collapse " id="sousmenutraite">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Crédit de trésorerie</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Crédit scolaire</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Microcrédit</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item" >
+                <a class="nav-link" href="#"><span class="oi oi-pencil" ></span> Payer un découvert</a>
+            </li>
+            <li class="nav-item" >
+                <a class="nav-link" href="#"><span class="oi oi-circle-check" ></span> Echéancier</a>
+            </li>
+        </ul>
+    </ul>
+
+    <ul class="nav nav-pills flex-column">
+
+            <a class="nav-link" href="#sousmenurapports" data-toggle="collapse" ><span class="oi oi-clipboard" ></span>&nbsp; Rapports</a>
+            <ul class="collapse " id="sousmenurapports">
+                <li class="nav-item">
+                    <a class="nav-link" href="#sousmenuetats" data-toggle="collapse" ><span class="oi oi-file" ></span> Etats des prêts</a>
+                    <ul class="collapse " id="sousmenuetats">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Crédit de Trésorerie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Crédit Scolaire</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Micro-crédits</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><span class="oi oi-arrow-right" ></span> Découverts</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="oi oi-document" ></span> Porte-feuille à risque</a>
+                </li>
+            </ul>
+
+    </ul>
+
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="test"><span class="oi oi-project" ></span>&nbsp; Général</a>
+        </li>
+    </ul>
+
+</nav>
