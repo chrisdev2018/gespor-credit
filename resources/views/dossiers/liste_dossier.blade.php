54bec906c2ec6a18a4e48076d330ca66eda49d63
Christian FOMEKONG <christian.fomekong@gmail.com>
2018-04-08 06:50:03 +0100

    [ADD] Ajout de la datatable au projet
    Tableau "liste_dossier" OK

--- resources/views/dossiers/liste_dossier.blade.php
+++ resources/views/dossiers/liste_dossier.blade.php
@@ -3,42 +3,52 @@
 @section('title', 'Dossiers Arrivés')
 
 
+@section('styles')
+    <link rel="stylesheet" href="assets/css/lib/datatable/dataTables.bootstrap.min.css">
+@endsection
+
+
 @section('content')
-
-    <h1>Liste des dossiers enregistrés</h1>
-
-    <div class="list-dossiers">
-        <table class="table table-info table-striped table-hover">
-            <thead class="thead-dark">
-            <tr >
-                <th>N° Compte</th>
-                <th>Noms & Prénoms</th>
-                <th>Type de credit</th>
-                <th>Montant demandé</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($membre as $mem)
-                @foreach($dossier_in as $dossier)
-                    @if($mem->id == $dossier->membre_id)
-                        <tr>
-                            <th>{{$mem->num_cpte}}</th>
-                            <th>{{$mem->nom}} {{$mem->prenom}}</th>
-                            <th>{{$dossier->type_credit}}</th>
-                            <th>{{$dossier->mnt_dmd}}</th>
-                            <th>
-                                <a  class="btn btn-info " title="Afficher plus de détails" data-toggle="modal"  data-target="#detailsModal{{$mem->id}}{{$dossier->id}}" ><i class="fa fa-eye"></i> Détails</a>&nbsp;
-                                <a id="update{{$dossier->id}}" class="btn btn-warning" data-toggle="modal" data-target="#updateModal{{$mem->id}}{{$dossier->id}}"   title="Modifier les informations"><i class="fa fa-edit"></i> Modifier</a>&nbsp;
-                                <a id="process{{$dossier->id}}" class="btn btn-primary" title="Traiter ce dossier" data-toggle="modal" data-target="#processModal{{$mem->id}}{{$dossier->id}}" ><i class="fa fa-cogs"></i> Traiter</a>
-                            </th>
-                        </tr>
-                    @endif
-                @endforeach
-            @endforeach
-            </tbody>
-        </table>
+    <div class="animated fadeIn">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">Liste de tous les dossiers enregistrés</strong>
+            </div>
+            <div class="card-body">
+                <table id="bootstrap-data-table" class="table table-bordered table-striped table-hover">
+                    <thead>
+                    <tr >
+                        <th>N° Compte</th>
+                        <th>Noms & Prénoms</th>
+                        <th>Type de credit</th>
+                        <th>Montant demandé</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($membre as $mem)
+                        @foreach($dossier_in as $dossier)
+                            @if($mem->id == $dossier->membre_id)
+                                <tr>
+                                    <td>{{$mem->num_cpte}}</td>
+                                    <td>{{$mem->nom}} {{$mem->prenom}}</td>
+                                    <td>{{$dossier->type_credit}}</td>
+                                    <td>{{$dossier->mnt_dmd}}</td>
+                                    <td>
+                                        <a  class="btn btn-info " title="Afficher plus de détails" data-toggle="modal"  data-target="#detailsModal{{$mem->id}}{{$dossier->id}}" ><i class="fa fa-eye"></i> Détails</a>&nbsp;
+                                        <a id="update{{$dossier->id}}" class="btn btn-warning" data-toggle="modal" data-target="#updateModal{{$mem->id}}{{$dossier->id}}"   title="Modifier les informations"><i class="fa fa-edit"></i> Modifier</a>&nbsp;
+                                        <a id="process{{$dossier->id}}" class="btn btn-primary" title="Traiter ce dossier" data-toggle="modal" data-target="#processModal{{$mem->id}}{{$dossier->id}}" ><i class="fa fa-cogs"></i> Traiter</a>
+                                    </td>
+                                </tr>
+                            @endif
+                        @endforeach
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
+
     <br><br>
 
 
@@ -81,4 +91,8 @@
       });
 
     </script>
+
+    @include('general.js_dt_including')
 @endsection
+
+
