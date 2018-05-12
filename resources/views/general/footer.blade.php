70a74abdf8b5bb4dc2af845c7c8c2109d175f73c
Christian FOMEKONG <christian.fomekong@gmail.com>
2018-05-12 12:23:04 +0100

    [IMP] footer

--- resources/views/general/footer.blade.php
+++ resources/views/general/footer.blade.php
@@ -1,3 +1,7 @@
 <div class="footer">
-      &copy; Copyright 2018.&nbsp;All Rights Reserved.
+      <br>
+      <div class="row justify-content-center"><img src="images/logo.png"></div>
+      <br>
+     <p>&copy; Copyright 2018.&nbsp;All Rights Reserved.</p>
+     <br>
 </div>
