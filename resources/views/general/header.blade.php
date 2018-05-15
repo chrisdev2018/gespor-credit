36c471045aa2dc736495119c25228aa58eeac8ce
Christian FOMEKONG <christian.fomekong@gmail.com>
2018-05-15 02:03:13 +0100

    [IMP] Système d'authentification

--- resources/views/general/header.blade.php
+++ resources/views/general/header.blade.php
@@ -2,11 +2,11 @@
 
     <div class="header-menu">
 
-        <div class="col-sm-7">
+        <div class="col-sm-9">
             <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa-compress"></i></a>
-
+<!--
             <div class="header-left">
-             <div class="dropdown for-notification">
+                <div class="dropdown for-notification">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-bell"></i>
                         <span class="count bg-danger">0</span>
@@ -27,32 +27,29 @@
                         </a>
                     </div>
                 </div>
-
-
             </div>
+-->
         </div>
-        <div class="col-sm-5">
 
-            <li class="dropdown">Conneté  :
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                    <strong>{{ Auth::user()->name }} </strong><span class="caret"></span>
-                </a>
+        <div class="col-sm-3 header-left">
+            <div class="dropdown for-notification">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-user"></i>&nbsp;&nbsp;
+                    <strong>{{ Auth::user()->name }} </strong>
+                    <i class="fa fa-sort-down"></i>
+                </button>
+                <div class="dropdown-menu" id="user-dropdown" aria-labelledby="notification">
+                    <a class="dropdown-item media " href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa fa-sign-out"></i>
+                        <p><strong>Se déconnecter</strong></p>
+                    </a>
 
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                </ul>
-            </li>
-
+                </div>
+            </div>
         </div>
     </div>
 
