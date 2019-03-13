c78e891ffcd61b30a0d94f948758726794df8356
Christian FOMEKONG <unknown@example.com>
2019-03-13 23:05:15 +0100

    [IMP] modification du footer

--- resources/views/general/footer.blade.php
+++ resources/views/general/footer.blade.php
@@ -2,6 +2,6 @@
       <br>
       <div class="row justify-content-center"><img src="images/logo.png"></div>
       <br>
-     <p>&copy; Copyright 2018.&nbsp;All Rights Reserved.</p>
+     <p>&copy; Copyright Christian 2018.&nbsp;All Rights Reserved. </p>
      <br>
 </div>
